671193b8b9032212780f9e2118ef12e37ec4e0da
Josué Jésugnon HOUNHUOI <hounhouijosue1@gmail.com>
2024-06-05 16:17:32 +0200

    update

---
+++ afriplume-app/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav className='nav'>
+          <div>
+            <img className='logo' src="./public/image.png" alt="Logo Afriplume" />
+          </div>
+          <div className='list'>
+            <ul>
+              <li>
+                Accueil
+              </li>
+              <li>
+                Genre Littéraire
+              </li>
+              <li>
+                Ecrire
+              </li>
+              <li>
+                Espace Auteur
+              </li>
+            </ul>
+          </div>
+      </nav>
+    </>
+  )
+}
+
+export default App
