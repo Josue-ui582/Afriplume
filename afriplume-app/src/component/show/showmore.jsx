abec8467ddbf4a0251d0471aeb9fbb852dbccd3c
Josué Jésugnon HOUNHUOI <hounhouijosue1@gmail.com>
2024-06-08 17:07:11 +0200

    update

---
+++ afriplume-app/src/component/show/showmore.jsx
@@ -0,0 +1,13 @@
+import './shomore.css';
+
+function Showmore() {
+    return (
+        <>
+            <div>
+                <a className="link" href="#">Afficher plus <i class='bx bxs-chevron-down'></i></a>
+            </div>
+        </>
+    )
+}
+
+export default Showmore
